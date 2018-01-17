5a38da5241e19fb98910598e6fa95bfe423f7a17
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-01-17 20:52:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,7 @@
-class Tic Tac Toe
+class TicTacToe
   def initialize
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-end
+  end
 
   WIN_COMBINATIONS = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -9,14 +9,14 @@ end
     [0, 4, 8], [2, 4, 6]
   ]
 
-def display_board
+  def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  
+
   def input_to_index(user_input)
     user_input.to_i - 1
   end
